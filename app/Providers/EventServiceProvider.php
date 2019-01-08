01e91d96e3c15e511f2938fed2d91b3eefbd1f81
Marc <zunly@hotmail.com>
2019-01-08 21:16:55 +0800

    中间件的回调

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Auth\Events\Verified;
+use \App\Listeners\EmailVerified;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Verified::class=>[
+            EmailVerified::class,
+        ]
     ];
 
     /**
