82eebc20c49def960079517db2750f6d0e1e7bc0
Marc <zunly@hotmail.com>
2019-01-08 20:52:44 +0800

    laravel自带的邮箱验证功能

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class PagesController extends Controller
 {
     public function root()
     {
+        //dd(\Auth::user()->hasVerifiedEmail());
         return view('pages.root');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable,MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
