bf3d65c1b56b6dcf999f1e75a3043f01ef51a3b4
Marc <zunly@hotmail.com>
2019-01-08 19:11:17 +0800

    修复了home路由, navbar的guest切换

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,5 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'PagesController@root')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
