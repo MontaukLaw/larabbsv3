3beb162e38fe338fdd6999dd27e4685d1a5e64ec
Marc <zunly@hotmail.com>
2019-01-10 22:18:21 +0800

    topicrequest

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -34,13 +36,21 @@ class TopicsController extends Controller
 
     public function create(Topic $topic)
     {
-        return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+
+        //return view('topics.create_and_edit', compact('topic'));
     }
 
-    public function store(TopicRequest $request)
+    public function store(TopicRequest $request, Topic $topic)
     {
-        $topic = Topic::create($request->all());
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        //$topic = Topic::create($request->all());
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+        //return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功');
+        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+
     }
 
     public function edit(Topic $topic)
--- app/helpers.php
+++ app/helpers.php
@@ -11,8 +11,13 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
-
 function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
