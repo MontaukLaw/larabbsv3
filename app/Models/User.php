eb748b1d4849d371516fa0dffead5ef4a262ddcd
Marc <zunly@hotmail.com>
2019-01-15 21:00:52 +0800

    用户注册及验证码接口

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar'
+        'name', 'email', 'password', 'introduction', 'avatar','phone'
     ];
 
     /**
