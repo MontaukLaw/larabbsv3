114000bf627de335290018e73804da6a8498a6c3
Marc <zunly@hotmail.com>
2019-01-17 16:26:06 +0800

    使用中间件的防止频繁访问的配置

--- routes/api.php
+++ routes/api.php
@@ -20,25 +20,32 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api) {
-    $api->get('version', function() {
+$api->version('v1', function ($api) {
+    $api->get('version', function () {
         return response('this is version v1');
     });
 });
 
-$api->version('v2', function($api) {
-    $api->get('version', function() {
+$api->version('v2', function ($api) {
+    $api->get('version', function () {
         return response('this is version v2');
     });
 });
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+], function ($api) {
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
