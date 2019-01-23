d78aeb3d88f628a97ed8b9f6d8aa6095f0112c85
Marc <zunly@hotmail.com>
2019-01-24 07:40:20 +0800

    清除已读通知数量

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -26,4 +26,9 @@ class NotificationsController extends Controller
             'unread_count' => $this->user()->notification_count,
         ]);
     }
+
+    public function read()
+    {
+        $this->user->markAsRead();
+    }
 }
