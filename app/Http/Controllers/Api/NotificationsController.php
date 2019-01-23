7807844502294bcfb2803b54b0b0eb8d08490bc9
Marc <zunly@hotmail.com>
2019-01-24 01:42:54 +0800

    用response->array拼数组的方法生成查询未读通知的接口

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Transformers\NotificationTransformer;
+use App\Transformers\UserTransformer;
 
 class NotificationsController extends Controller
 {
@@ -12,5 +13,17 @@ class NotificationsController extends Controller
         $notifications = $this->user->notifications()->paginate(20);
 
         return $this->response->paginator($notifications, new NotificationTransformer());
+    }
+
+    public function stats()
+    {
+        //$user = $this->user;
+        ////$userStats=$user->notification_count;
+        /// //return $this->response->item($userStats)
+        /// //->setStatusCode(201);
+
+        return $this->response->array([
+            'unread_count' => $this->user()->notification_count,
+        ]);
     }
 }
